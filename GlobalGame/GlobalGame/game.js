d0c414b5db8fabbe8ffab1ac6072cc0295253cf1
300024658 <malambe4658@fvtc.edu>
2017-02-03 13:37:33 -0600

    sort the code to understand better

--- GlobalGame/GlobalGame/game.js
+++ GlobalGame/GlobalGame/game.js
@@ -15,6 +15,8 @@
         var tickCount = 0;
         var currentArea = null;
 
+        var shape = new Array();
+
         var xCord = 0;
         var yCord = 0;
         var colors = ["pink", "blue", "red", "orange"];
@@ -29,30 +31,32 @@
                 circle.x =  xCord;
                 circle.y =  yCord;
                 yCord += 10;
-                
-                var dragger = new createjs.Container();
-             //   dragger.x = dragger.y = 10;
-                dragger.addChild(circle);
-                stage.addChild(dragger);
-                dragger.on("mousedown", function (evt) {
-                    //a record on the offset between the mouse position and the container
-                    // position. currentTarget will be the container that the event listener was added to:
-                    evt.currentTarget.offset = { x: this.x - evt.stageX, y: this.y - evt.stageY };
-                });
-                dragger.on("pressmove", function (evt) {
-                    // Calculate the new X and Y based on the mouse new position plus the offset.
-                    evt.currentTarget.x = evt.stageX + evt.currentTarget.offset.x;
-                    evt.currentTarget.y = evt.stageY + evt.currentTarget.offset.y;
+                shape(x) = circle;
 
-                });
             }
             //
             yCord = 0;
             xCord += 10;
-            stage.update();
-            createjs.Ticker.addEventListener("tick", stage);
         }
-      
+
+            var dragger = new createjs.Container();
+            //   dragger.x = dragger.y = 10;
+            dragger.addChild(circle);
+            stage.addChild(dragger);
+            dragger.on("mousedown", function (evt) {
+                //a record on the offset between the mouse position and the container
+                // position. currentTarget will be the container that the event listener was added to:
+                evt.currentTarget.offset = { x: this.x - evt.stageX, y: this.y - evt.stageY };
+            });
+            dragger.on("pressmove", function (evt) {
+                // Calculate the new X and Y based on the mouse new position plus the offset.
+                evt.currentTarget.x = evt.stageX + evt.currentTarget.offset.x;
+                evt.currentTarget.y = evt.stageY + evt.currentTarget.offset.y;
+
+            });
+         
+        stage.update();
+        createjs.Ticker.addEventListener("tick", stage);
         
     }
     return Game;
