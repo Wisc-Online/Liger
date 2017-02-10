2b94cfea36923fb65f306c54a97be51b72d88af3
Marina Schiegg <schiegg@fvtc.edu>
2017-02-10 12:30:58 -0600

    gamejs refactoring

--- GlobalGame/GlobalGame/game.js
+++ GlobalGame/GlobalGame/game.js
@@ -1,10 +1,11 @@
-﻿var Game = Game || (function (createjs, $) {
+﻿/// <reference path="C:\SoftwareDev\GIT\Liger\GlobalGame\GlobalGame\createjs.min.js" />
+var Game = Game || (function (createjs, $) {
     function Game(canvasId, gameData) {
-        
+
 
 
         //var assetsPath = gameData.assetsPath || "";
-        gameData = gameData || {};
+        var gameData = gameData || {};
         var self = this;
         var mouseBp;
         var stage = new createjs.Stage(canvasId);
@@ -16,77 +17,314 @@
         var tickCount = 0;
         var currentArea = null;
 
-        var board = prepareBoard(createjs);
-        drawBoard(createjs, stage, board);
-        createjs.Ticker.addEventListener("tick", stage);
-        //code swapping elements in array
-        //drawBoard(createjs, stage, board);
+        //set ticker 
+        createjs.Ticker.setFPS(60);
 
+        function handleTick() {
+            stage.update();
+        }
+
+        createjs.Ticker.on("tick", handleTick);
+
+
+
+
+
+        function initialize() {
+
+            //create game objects
+            var mainBox, questionContainer, userScoreContainer, layer, rectangle;
+
+            // Main game box
+            
+            mainBox = createMainContainer();
+            fillBoard();
+
+            //add terms library container
+            questionContainer = createquestionContainer();
+            questionContainer.x = 580;
+            questionContainer.y = 40;
+
+
+            //add user score container
+            userScoreContainer = createUserScoreContainer();
+            userScoreContainer.x = 580;
+            userScoreContainer.y = 265;
+
+
+            // adding elements to stage
+            stage.addChild(mainBox, userScoreContainer, questionContainer, layer, rectangle);
+
+
+            function createMainContainer() {
+
+                var container = new createjs.Container();
+
+                var background = new createjs.Shape();
+                background.graphics.setStrokeStyle(1).beginStroke("black").beginFill("aqua");
+                background.graphics.drawRect(20, 20, 400, 400);
+
+
+                container.addChild(background);
+                return container;
+            }
+
+            function createCircleDraggableContainer(xCord,yCord) {
+
+                
+
+                var maxWidth = 40;
+
+                //library of terms
+                var container = new createjs.Container();
+
+                var background = new createjs.Shape();
+                background.graphics.setStrokeStyle(1).beginStroke("black").beginFill("white");
+                background.graphics.drawRect(0, 0, maxWidth, 40);
+                container.setBounds(0, 0, maxWidth, 40);
+
+                var colors = ["pink", "blue", "red", "orange"];
+                var randomNum = Math.floor((Math.random() * 4) + 0);
+                var circle = new createjs.Shape();
+                circle.graphics.beginFill(colors[randomNum]).drawCircle(maxWidth / 2, maxWidth / 2, maxWidth / 2);
+
+
+
+                container.on("pressmove", handleTermDrag);
+                container.on("pressup", handleTermPressUp);
+
+                var mouseDragPosition = null;
+
+                container.addChild(background);
+                container.addChild(circle);
+
+
+
+                var isDragging = false;
+
+                //drag functionality
+                function handleTermDrag(evt) {
+
+                    if (mouseDragPosition != null) {
+                        var deltaX = evt.stageX - mouseDragPosition.x;
+                        var deltaY = evt.stageY - mouseDragPosition.y;
+
+
+                        var dragThreshold = 30;
+                        var iIndex = evt.currentTarget.i;
+                        var jIndex = evt.currentTarget.j;
+                        if (isDragging) {
+                            
+
+                            if (deltaX > dragThreshold && iIndex < 9) {
+                                // move right
+
+                                
+
+                                var rightCircle = gameData[iIndex + 1][jIndex];
+
+                                var xx = evt.currentTarget.x;
+                                createjs.Tween.get(evt.currentTarget).to({ x: rightCircle.x }, 200);
+                                createjs.Tween.get(rightCircle).to({ x: xx }, 200);
+
+                                
+                                evt.currentTarget.i = iIndex + 1;
+                                rightCircle.i = iIndex;
+
+                                gameData[iIndex + 1][jIndex] = evt.currentTarget;
+                                gameData[iIndex][jIndex] = rightCircle;
+
+
+                                mouseDragPosition = null;
+                                isDragging = false;
+                            }
+                            else if (deltaX < -dragThreshold && iIndex > 0) {
+                                // move left
+                                var leftCircle = gameData[iIndex - 1][jIndex];
+                                var xx = evt.currentTarget.x;
+                                createjs.Tween.get(evt.currentTarget).to({ x: leftCircle.x }, 200);
+                                createjs.Tween.get(leftCircle).to({ x: xx }, 200);
+
+                                evt.currentTarget.i = iIndex - 1;
+                                leftCircle.i = iIndex;
+
+
+                                gameData[iIndex - 1][jIndex] = evt.currentTarget;
+
+                                gameData[iIndex][jIndex] = leftCircle;
+
+                                mouseDragPosition = null;
+                                isDragging = false;
+                            }
+                            else if (deltaY < -dragThreshold && jIndex > 0) {
+                                // move up
+                                var topCircle = gameData[iIndex][jIndex - 1];
+                                var yy = evt.currentTarget.y;
+                                createjs.Tween.get(evt.currentTarget).to({ y: topCircle.y }, 200);
+                                createjs.Tween.get(topCircle).to({ y: yy }, 200);
+
+
+                                evt.currentTarget.j = jIndex - 1;
+                                topCircle.j = jIndex;
+
+
+                                gameData[iIndex][jIndex - 1] = evt.currentTarget;
+
+                                gameData[iIndex][jIndex] = topCircle;
+
+                                mouseDragPosition = null;
+                                isDragging = false;
+                            }
+                            else if (deltaY > dragThreshold && jIndex < 9) {
+                                // move down
+                                var bottomCircle = gameData[iIndex][jIndex + 1];
+                                var yy = evt.currentTarget.y;
+                                createjs.Tween.get(evt.currentTarget).to({ y: bottomCircle.y }, 200);
+                                createjs.Tween.get(bottomCircle).to({ y: yy }, 200);
+
+                                evt.currentTarget.j = jIndex + 1;
+                                bottomCircle.j = jIndex;
+
+                                gameData[iIndex][jIndex + 1] = evt.currentTarget;
+
+                                gameData[iIndex][jIndex] = bottomCircle;
+
+                                
+
+
+                                mouseDragPosition = null;
+                                isDragging = false;
+                            }
+                        }
+                        else {
+                            isDragging = true;
+                        }
+                    }
+                    else
+                    {
+                        mouseDragPosition = {
+                            x: evt.stageX,
+                            y: evt.stageY
+                        };
+
+                    }
+                   
+                 
+                }
+
+                //determine if term is outside mainbox and return to terms library container
+                function handleTermPressUp(evt) {
+
+
+                    mouseDragPosition = null;
+
+                  
+             
+                }
+
+                return container;
+            }
+
+            function fillBoard()
+            {
+                var xCord = 20;
+                for (var i = 0; i < 10; i++) {
+                    var yCord = 20;
+                    gameData[i] = [];
+                    for (var j = 0; j < 10; j++) {
+                        var circle = createCircleDraggableContainer();
+                        circle.x = xCord;
+                        circle.y = yCord;
+                        circle.original_x = circle.x;
+                        circle.original_y = circle.y;
+                        mainBox.addChild(circle);
+
+                        circle.i = i;
+                        circle.j = j;
+                        gameData[i][j] = circle;
+                        yCord += 40;
+                    }
+                    
+                    xCord += 40;
+                }
+
+
+                
+
+
+                
+            }
+            
+
+            //////////////////////////////////////////
+
+            // terms library container
+            function createquestionContainer() {
+
+                //library container
+                var container = new createjs.Container();
+
+                //library background
+                var background = new createjs.Shape();
+                background.graphics.setStrokeStyle(1).beginStroke("black").beginFill("yellow");
+                background.graphics.drawRect(0, 0, 100, 210);
+                container.addChild(background);
+
+                var offset_x, offset_y;
+                var numberOfItemsPerColumn = 5;
+                var padding = 5;
+
+              
+
+                return container;
+            }
+
+
+
+            function createUserScoreContainer() {
+                //user score container
+                var container = new createjs.Container();
+
+                //user score background
+                var background = new createjs.Shape();
+                background.graphics.setStrokeStyle(1).beginStroke("black").beginFill("blue");
+                background.graphics.drawRect(0, 0, 100, 50);
+                container.addChild(background);
+
+                //user score title
+                var scoreLabel = new createjs.Text("", "15px Verdana", "");
+                scoreLabel.color = "white";
+                scoreLabel.text = "Score:";
+                scoreLabel.x = 25;
+                scoreLabel.y = 2;
+                container.addChild(scoreLabel);
+
+                //user score score
+                var scoreText = new createjs.Text("", "20px Verdana", "");
+                scoreText.color = "orange";
+                scoreText.text = 900; //this will need to change later to be a var to hold user score. 
+                scoreText.x = 30;
+                scoreText.y = 20;
+                container.addChild(scoreText);
+                return container;
+            }
+
+
+
+        }
+
+        function reset() {
+
+            stage.removeAllChildren();
+            initialize();
+
+        }
+
+        initialize();
 
         
     }
     return Game;
 })(createjs, $);
 
-function prepareBoard(createjs) {
-    var xCord = 20;
-    var yCord = 20;
-    var colors = ["pink", "blue", "red", "orange"];
-    var board = [];
 
-    for (x = 0; x < 10; x++) {
-        board[x] = [];
-        for (y = 0; y < 10; y++) {
-            
-            var randomNum = Math.floor((Math.random() * 4) + 0);
-            //var circle = { xCord: xCord, yCord: yCord, color: colors[randomNum] };
-            var circle = new createjs.Shape();
-            circle.graphics.beginFill(colors[randomNum]).drawCircle(xCord, yCord, 20);
-
-
-            circle.allowedOffset = { xLeft: xCord - 40, xRight: xCord + 40, yUp: yCord - 40, yDown: yCord + 40 }
-
-
-            circle.on("mousedown", function (evt) {
-                this.parent.addChild(this);
-                this.offset = { x: this.x - evt.stageX, y: this.y - evt.stageY };
-            });
-
-
-            circle.on("pressmove", function (evt) {
-
-                if ((evt.stageX + this.offset.x) >= this.allowedOffset.xLeft && (evt.stageX + this.offset.x) <= this.allowedOffset.xRight
-                    && (evt.stageY + this.offset.y) >= this.allowedOffset.yUp && (evt.stageY + this.offset.y) <= this.allowedOffset.yDown)
-                {
-                    this.x = evt.stageX + this.offset.x;
-                    this.y = evt.stageY + this.offset.y;
-                }
-
-            });
-            board[x][y] = circle;
-            yCord += 40;
-        }
-
-        yCord = 20;
-        xCord += 40;
-
-    }
-    return board;
-}
-
-function drawBoard(createjs,stage,board)
-{
-
-    for (x = 0; x < 10; x++) {
-        for (y = 0; y < 10; y++) {
-
-            stage.addChild(board[x][y]);
-
-        }
-    }
-    stage.update();
-}
-
-function handleInteraction(event) {
-    event.target.alpha = (event.type == "mouseover") ? 0.5 : 1;
-}
