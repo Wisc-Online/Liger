43c87493ca96997348c8e4aa01f64e32f215e87a
T Thiel <thielt@fvtc.edu>
2017-02-16 15:04:17 -0600

    js file change

---
+++ TomsGame/TomsGame/game - jetpack.js
@@ -0,0 +1,142 @@
+﻿// can put this in html  <body onload="Main();"> 
+
+/// <reference path="createjs-2015.11.26.min.js" />
+// JavaScript Document
+// window.onload = function() {
+
+    // assume Game Data was created by an external builder being passed into this script
+    
+
+        /* Background */
+    var bgImg = new Image();
+    var bg1;
+    var bg2Img = new Image();
+    var bg2;
+
+        /* Jetpack */ 
+        var sImg = new Image();
+        var ship;
+   
+
+        /* Enemy */
+    var eImg = new Image() ;
+
+        /* planet(s) */
+    var bImg = new Image() ;
+    var boss;
+
+        /* Fuel Crystals | Lives */
+    var lImg = new Image(); // was lives lImg
+
+        // variables
+  //  var lives = new Container(); //stores the lives gfx 
+  //  var bullets = new Container(); //stores the bullets gfx 
+ //   var enemies = new Container(); //stores the enemies gfx 
+    var bossHealth = 20;
+    var score;
+    var gfxLoaded = 0; //used as a preloader, counts the already loaded items 
+    var centerX = 160;
+    var centerY = 240;
+    var tkr = new Object(); //used as a Ticker listener 
+    var timerSource; //references a setInterval method
+
+           
+
+            //set ticker 
+    // createjs.Ticker.setFPS(60);
+   // createjs.Ticker.on("tick", handleTick)
+
+    function Main()
+{
+        //code... 
+    var canvas = document.getElementById("myCanvas");
+    var stage = new createjs.Stage(canvas);
+        stage.mouseEventsEnabled = true;
+
+        /* Sounds */
+  
+  //  SoundJS.addBatch([ 
+   //     {name:'boss', src:'sounds/game-die.mp3', instances:1}, 
+    //    {name:'explo', src:'deflate.mp3', instances:10}, 
+    //    {name:'shot', src:'rf_Balloon_Pop.mp3', instances:10}]);
+
+        /* Load GFX */  
+        bgImg.src = 'Images/bg1.png'; 
+        bgImg.name = 'bg'; 
+        bgImg.onload = loadGfx; 
+      
+        bg2Img.src = 'Images/bg2.png'; 
+        bg2Img.name = 'bg2'; 
+        bg2Img.onload = loadGfx; 
+      
+        sImg.src = 'Images/thumbsUpMan.png'; // ship or main character
+        sImg.name = 'ship'; 
+        sImg.onload = loadGfx; 
+  
+        eImg.src = 'Images/bag_creature.png';  // alien or zombie enemy
+        eImg.name = 'enemy'; 
+        eImg.onload = loadGfx; 
+  
+        bImg.src = 'Images/planet-1138237.png';  // boss or planet *different planet sprites are in the repository
+        bImg.name = 'boss'; 
+        bImg.onload = loadGfx; 
+
+        /* Ticker */  
+     //   Ticker.setFPS(30); 
+    //    Ticker.addListener(stage);
+
+     /* Ticker */
+    //   createjs.Ticker.setFPS(30);
+    //    createjs.Ticker.addListener(stage);
+               
+              
+}
+
+function loadGfx(e) 
+{ 
+  //  if(e.target.name = 'bg'){bg = new Bitmap(bgImg);} 
+ //   if(e.target.name = 'bg2'){bg2 = new Bitmap(bg2Img);} 
+    if(e.target.name = 'ship'){ship = new Bitmap(sImg);} 
+      
+    gfxLoaded++; 
+      
+    if(gfxLoaded == 9) 
+    { 
+        addGameView(); 
+    } 
+}
+
+function addGameView() 
+{ 
+    ship.x = centerX - 18.5; 
+    ship.y = 480 + 34; 
+      
+    /* Add Lives */
+      
+    //for(var i = 0; i < 3; i++) 
+    //{ 
+    //    var l = new Bitmap(lImg); 
+          
+    //    l.x = 248 + (25 * i); 
+    //    l.y = 463; 
+          
+    //    lives.addChild(l); 
+    //    stage.update(); 
+    //} 
+      
+    /* Score Text */
+      
+    score = new Text('0', 'bold 14px Courier New', '#FFFFFF'); 
+    score.maxWidth = 1000;  //fix for Chrome 17 
+    score.x = 2; 
+    score.y = 476; 
+      
+    /* Second Background */
+      
+    bg2.y = -480; 
+      
+    /* Add gfx to stage and Tween Ship */
+      
+    stage.addChild(bg, bg2, ship, enemies, bullets, lives, score); 
+    Tween.get(ship).to({y:425}, 1000).call(startGame); 
+}
--- TomsGame/TomsGame/game.js
+++ TomsGame/TomsGame/game.js
@@ -1,105 +1,214 @@
-﻿/// <reference path="createjs-2015.11.26.min.js" />
+﻿
+/// <reference path="https://code.createjs.com/createjs-2015.11.26.min.js" />
 // JavaScript Document
-// window.onload = function() {
+window.onload = function () {
 
     // assume Game Data was created by an external builder being passed into this script
-    
+    var theCanvas = document.getElementById("myCanvas");
+    var stage = new createjs.Stage(theCanvas);
+    var playerState = "normal"; // normal, turning, zombie
+    var vaccine; // does player have a vaccine on hand
+    // var zombie = new createjs.Shape();
+    var zombie = new createjs.Bitmap("Images/bag_creature.png");
 
-        /* Background */
-    var bgImg = new Image();
-    var bg1;
-    var bg2Img = new Image();
-    var bg2;
 
-        /* Jetpack */ 
-        var sImg = new Image();
-        var ship;
+    var assets = [
+           { id: "DesertBackground",src:"Images/HighDefForgroundDesert.png"} // can put all assets here ,
+           
+    ];
+
+    var queue = new createjs.LoadQueue(false);
+    queue.installPlugin(createjs.Sound);
+    queue.addEventListener("complete", function (event) {
+        initializeGame();
+    });
+    queue.loadManifest(assets);
+
+    //set ticker 
+    createjs.Ticker.setFPS(60);
+    createjs.Ticker.on("tick", handleTick)
+
+      
+    //container for stage objects
+    function initialize() {
+        //load sounds
+        createjs.Sound.registerSound("sounds/jump_sound.wav", "jump");
+        createjs.Sound.registerSound("sounds/thud.wav", "thud");
+        createjs.Sound.registerSound("sounds/game-die.mp3", "die");
+      //  createjs.Sound.addEventListener("LoadComplete", handleComplete);
+
+        // Main game box
+        var mainBox = new createjs.Shape();
+        mainBox.x = 120;
+        mainBox.y = 20;
+        mainBox.graphics.setStrokeStyle(1).beginStroke("black").beginFill("darkgrey");
+        mainBox.graphics.drawRect(25, 0, 500, 560);
+        // adding mainbox to stage
+        stage.addChild(mainBox);
+        
+
+        // main character
+        // var player = new createjs.Shape();
+        var player = new createjs.Bitmap("Images/thumbsUpMan.png");
+        // player.graphics.beginFill("blue").drawCircle(0, 0, 25);
+        player.x = 375;
+        player.y = 450;
+        player.scaleX = .10;
+        player.scaleY = .10;
+        
+        player.addEventListener("click", handleClick); //mouse click or screen tap
+            
+        // add character to stage
+        stage.addChild(player);
+
+       
+        // zombie character
+        zombie.addEventListener("click", handleZombieClick); //mouse click or screen tap
+        zombie.scaleX = .75;
+        zombie.scaleY = .75;
+        zombie.x = 400;
+        zombie.y = 20;
+        stage.addChild(zombie);
+
+        createjs.Tween.get(zombie, { loop: true })
+      .to({ x: 600 }, 1000, createjs.Ease.getPowInOut(3))
+      .to({ alpha: 1, y: 175 }, 525, createjs.Ease.getPowInOut(2))
+      .to({ x: 400 }, 1000, createjs.Ease.getPowInOut(4))
+      .to({ alpha: 1, y: 225 }, 500, createjs.Ease.getPowInOut(2))
+      .to({ x: 200 }, 1000, createjs.Ease.getPowInOut(5))
+      .to({ alpha: 1, y: 275 }, 475, createjs.Ease.getPowInOut(2))
+      .to({ x: 400 }, 900, createjs.Ease.getPowInOut(2))
+      .to({ alpha: 1, y: 325 }, 450, createjs.Ease.getPowInOut(2))
+      .to({ x: 600 }, 800, createjs.Ease.getPowInOut(3))
+      .to({ alpha: 1, y: 400 }, 425, createjs.Ease.getPowInOut(2))
+      .to({ x: 200 }, 700, createjs.Ease.getPowInOut(3))
+      .to({ alpha: 1, y: 540 }, 400, createjs.Ease.getPowInOut(2))
+      .to({ x: 800 }, 2000, createjs.Ease.getPowInOut(2));
+
+        //check for collision
+        if (player.x < zombie.x + zombie.width && player.x + player.width > zombie.x &&
+          player.y < zombie.y + zombie.height && player.y + player.height > zombie.y) {
+            // The objects are touching
+            console.log("you've been bitten!");
+           
+        }
+        
+
+        var checkRectCollision = function (player, zombie) {
+            var b1, b2;
+            b1 = getBounds(player);
+            b2 = getBounds(zombie);
+            return calculateIntersection(b1, b2);
+        }
+
+        if (checkRectCollision != null) {
+            console.log("you've been bitten!");
+        };
+        
+    }
+
+    //main game view
+    function createMainGameView() {
+        mainView = new createjs.Container();
+        mainView.name = "MainView";
+        var background = new createjs.Bitmap(queue.getResult("DesertBackground"));
+        mainView.addChild(background);
+        currentArea = getDisplayQuestionArea();
+        var viewAnswersContainer = new createjs.Container();
+
+        mainView.addChild(viewAnswersContainer);
+        ScoreLabel = new createjs.Text("Score", "14px Arial bold", "white");
+        ScoreLabel.x = 725;
+        ScoreLabel.y = 10;
+
+        ScoreText = new createjs.Text(gameState.score.toString(), "18px Arial bold", "white");
+        ScoreText.x = 715;
+        ScoreText.y = 50;
+
+        TimerLabel = new createjs.Text("Time", "14px Arial bold", "white");
+        TimerLabel.x = 550;
+        TimerLabel.y = 20;
+
+        Timer = new createjs.Text("Test", "18px Arial bold", "white");
+        Timer.x = 630;
+        Timer.y = 15;
+        Timer.visible = false;
+        gameState.timer = timeLimit;
+
+        ShotsLabel = new createjs.Text("Shots", "14px Arial bold", "white");
+        ShotsLabel.x = 550;
+        ShotsLabel.y = 60;
+        Shots = new createjs.Text(currentstate.shotsLeft.toString(), "14px Arial bold", "white");
+        Shots.x = 648;
+        Shots.y = 60;
+
+        mainView.addChild(Timer, TimerLabel, ScoreLabel, ShotsLabel, Shots, ScoreText);
+        mainView.addChild(currentArea);
+       //ar soundStuff = stage.getChildByName("theSoundContainer");
+       //oundStuff.visible = false;
+
+        return mainView;
+    }
+
+    function handleComplete(event) {
+
+    }
    
 
-        /* Enemy */
-    var eImg = new Image() ;
+    function handleTick() {
+        stage.update();
+    }
 
-        /* planet(s) */
-    var bImg = new Image() ;
-    var boss;
+    function handleClick(event) {
+        createjs.Sound.play("jump");
+       // player.graphics.beginFill("yellow").drawzombie(0, 0, 25);
+        
+        jumpUp();
+    }
 
-        /* Fuel Crystals | Lives */
-    var lImg = new Image(); // was lives lImg
+    function handleZombieClick(event) {
+        createjs.Sound.play("die");
+        createjs.Tween.get(zombie).to({alpha: 0},5000);
+        stage.removeChild(zombie);
+        
+        //  stage.removeChild(zombie[i]);
+        // player.graphics.beginFill("yellow").drawzombie(0, 0, 25);
 
-        // variables
-  //  var lives = new Container(); //stores the lives gfx 
-  //  var bullets = new Container(); //stores the bullets gfx 
- //   var enemies = new Container(); //stores the enemies gfx 
-    var bossHealth = 20;
-    var score;
-    var gfxLoaded = 0; //used as a preloader, counts the already loaded items 
-    var centerX = 160;
-    var centerY = 240;
-    var tkr = new Object(); //used as a Ticker listener 
-    var timerSource; //references a setInterval method
+       
+    }
+     
+    function jumpUp(event) {
+       
+        createjs.Tween.get(player)
+       .to({ alpha: 1, y: player.y - 100 }, 200, createjs.Ease.circInOut(2))
+        .to({ alpha: 1, y: player.y }, 500, createjs.Ease.bounceOut(30))
+        .wait(150).call(function()
+        {   //jumpin landing thud
+            createjs.Sound.play("thud");
+        })
+       
+    }
 
-           
+    function zombieEncounter() {
+        
+        var randomNumber = Math.round(Math.random());
+        if (randomNumber === 0) {
+            alert("You have been bitten!");
+            //present question to player. if correct use a vaccine if one has been found or start timer to find a vaccine. If incorrect start "turning" green?
+        }
+        else if (randomNumber === 1) {
+            alert("You fought off the zombie!");
+            // you find a vaccine to keep or use if already "turning".
+        }
+    }
 
-            //set ticker 
-    // createjs.Ticker.setFPS(60);
-   // createjs.Ticker.on("tick", handleTick)
+    //reset button functionality
+    function reset() {
 
-    function Main()
-{
-        //code... 
-    var canvas = document.getElementById("myCanvas");
-    var stage = new createjs.Stage(canvas);
-        stage.mouseEventsEnabled = true;
+        stage.removeAllChildren();
+        initialize();
 
-        /* Sounds */
-  
-  //  SoundJS.addBatch([ 
-   //     {name:'boss', src:'sounds/game-die.mp3', instances:1}, 
-    //    {name:'explo', src:'deflate.mp3', instances:10}, 
-    //    {name:'shot', src:'rf_Balloon_Pop.mp3', instances:10}]);
+    }
 
-        /* Load GFX */  
-        bgImg.src = 'Images/bg1.png'; 
-        bgImg.name = 'bg'; 
-        bgImg.onload = loadGfx; 
-      
-        bg2Img.src = 'Images/bg2.png'; 
-        bg2Img.name = 'bg2'; 
-        bg2Img.onload = loadGfx; 
-      
-        sImg.src = 'Images/thumbsUpMan.png'; // ship or main character
-        sImg.name = 'ship'; 
-        sImg.onload = loadGfx; 
-  
-        eImg.src = 'Images/bag_creature.png';  // alien or zombie enemy
-        eImg.name = 'enemy'; 
-        eImg.onload = loadGfx; 
-  
-        bImg.src = 'Images/planet-1138237.png';  // boss or planet *different planet sprites are in the repository
-        bImg.name = 'boss'; 
-        bImg.onload = loadGfx; 
-
-        /* Ticker */  
-     //   Ticker.setFPS(30); 
-    //    Ticker.addListener(stage);
-
-     /* Ticker */
-    //   createjs.Ticker.setFPS(30);
-    //    createjs.Ticker.addListener(stage);
-               
-
-}
-
-function loadGfx(e) 
-{ 
-    if(e.target.name = 'bg'){bg = new Bitmap(bgImg);} 
-    if(e.target.name = 'bg2'){bg2 = new Bitmap(bg2Img);} 
-    if(e.target.name = 'ship'){ship = new Bitmap(sImg);} 
-      
-    gfxLoaded++; 
-      
-    if(gfxLoaded == 9) 
-    { 
-        addGameView(); 
-    } 
+    initialize();
 }
