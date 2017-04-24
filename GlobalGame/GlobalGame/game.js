7e5e25273b2d8b1f6f1a9d6fff315237b37f47c1
JULIET MALAMBE <malambe3@gmail.com>
2017-04-24 16:02:02 -0500

    //

--- GlobalGame/GlobalGame/game.js
+++ GlobalGame/GlobalGame/game.js
@@ -1096,7 +1096,11 @@ var Game = Game || (function (createjs, $) {
 
                   ac.text = question.Answers[i].Text;
                   ac.IsCorrect = question.Answers[i].IsCorrect;
+                
+
                   container.addChild(ac);
+
+
               }
               if (mainBox)
                 stage.setChildIndex(container, mainBox.getNumChildren() - 1);
@@ -1114,9 +1118,10 @@ var Game = Game || (function (createjs, $) {
                        
                         var answerText = new createjs.Text("", "16px Verdana", "");
                         answerText.color = "black";
-                        answerText.text = "The correct answer is " + gameData.Questions[currentQuestion].Answers[k].Text + " "  + gameData.Questions[currentQuestion].Answers[k].Details;
+                        answerText.text = "The correct answer is " + gameData.Questions[currentQuestion].Answers[k].Text + " : \n\n"  + gameData.Questions[currentQuestion].Answers[k].Details;
                         answerText.x = 0;
                         answerText.y = 150;
+    
                         answerText.lineWidth = 300;
                         answerText.name = "detailsText";
                         ad.name = "details";
